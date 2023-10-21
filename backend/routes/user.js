c5b923c58108d3d83653fae8b2e4085d1e39e480
bq0198 <chabelly.lbetancourt@alumnos.upm.es>
2023-10-21 19:21:54 +0200

    Users roles

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,7 +10,7 @@ router.use(validateJWT);
 router.get('/list-users', listUsers);
 router.post('/change-to-admin/:userId', changeToAdmin);
 router.post('/change-to-current/:userId', changeToCurrent);
-router.post('/change-account-status-to-Blocked/:userId', changeAccountStatusToBlocked);
+router.post('/change-account-status-to-blocked/:userId', changeAccountStatusToBlocked);
 router.post('/change-account-status-to-active/:userId', changeAccountStatusToActive);
 
 module.exports = router;
