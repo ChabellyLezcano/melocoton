f1c891365adb47f355942fb0b89c115e9adc05d4
bq0198 <chabelly.lbetancourt@alumnos.upm.es>
2023-11-19 23:46:28 +0100

    Deleted unused styles files

--- frontend/src/app/shared/components/reservation-item/reservation-item.component.ts
+++ frontend/src/app/shared/components/reservation-item/reservation-item.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-reservation-item',
-  templateUrl: './reservation-item.component.html',
-  styleUrls: ['./reservation-item.component.css']
+  templateUrl: './reservation-item.component.html'
 })
 export class ReservationItemComponent {
 
